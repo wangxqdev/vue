7c1169a8eb629206a5119dbf62b693755afa5ea4
wangxinquan <825877689@qq.com>
2020-03-14 17:07:02 +0800

    初识webpack

---
+++ 21-webpack/src/main.js
@@ -0,0 +1,8 @@
+// Common JS
+// const {add, mul} = require("./math.js");
+
+// ES6(推荐)
+import {add, mul} from "./math"
+
+console.log(add(20, 30));
+console.log(mul(20, 30));
---
+++ 21-webpack/src/math.js
@@ -0,0 +1,16 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function mul(num1, num2) {
+  return num1 * num2;
+}
+
+// Common JS
+// module.exports = {
+//   add, mul
+// }
+
+// ES6(推荐)
+export {add, mul}
+
