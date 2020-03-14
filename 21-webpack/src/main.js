85ecbf95f5a382ff367a80475c2384138245e49f
wangxinquan <825877689@qq.com>
2020-03-14 18:07:46 +0800

    webpack配置css

--- 21-webpack/src/main.js
+++ 21-webpack/src/main.js
@@ -2,7 +2,8 @@
 // const {add, mul} = require("./math.js");
 
 // ES6(推荐)
-import {add, mul} from "./math"
+import {add, mul} from "./js/math.js";
+import normal from "./css/normal.css"
 
 console.log(add(20, 30));
 console.log(mul(20, 30));
