6491335d79e8e5b374de29219c95a6724ec7018a
wangxinquan <825877689@qq.com>
2020-03-19 21:19:36 +0800

    vue-router初体验

---
+++ 25-router/src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '../components/Home'
+import About from '../components/About'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/about',
+    component: About
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
