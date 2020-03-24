7983e8dee6cd69c3d341bbdb79e0aa3ab1f77231
wangxinquan <wangxinquan@anji-tec.com>
2020-03-24 15:10:23 +0800

    modules的基本使用

--- 28-vuex/src/store/index.js
+++ 28-vuex/src/store/index.js
@@ -54,5 +54,21 @@ export default new Vuex.Store({
     gtPriceBooks(state, getters) {
       return price => state.books.filter(book => book.price > price)
     }
+  },
+  modules: {
+    a: {
+      state: {
+        // 特殊 => 通过 this.$store.state.a.[prop] 调用
+      },
+      actions: {
+        // 通过 this.$store.dispatch 调用
+      },
+      mutations: {
+        // 通过 this.$store.commit 调用
+      },
+      getters: {
+        // 通过 this.$store.getters.[prop] 调用
+      }
+    }
   }
 })
