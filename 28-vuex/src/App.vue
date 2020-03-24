c27c46d07754189eaeb3107486d348f8e246ee0e
wangxinquan <wangxinquan@anji-tec.com>
2020-03-24 13:36:21 +0800

    state、getters和mutations基本使用

--- 28-vuex/src/App.vue
+++ 28-vuex/src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h2>---------- APP ----------</h2>
+    <h2>{{$store.state.counter}}</h2>
+    <button @click="add">＋</button>
+    <button @click="sub">－</button>
+    <button @click="addBook">添加书籍</button>
+    <ul>
+      <li v-for="item in $store.getters.gtPriceBooks(88)" :key="item.id">{{item.name}}</li>
+    </ul>
+    <h2>---------- HelloVuex ----------</h2>
+    <hello-vuex></hello-vuex>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloVuex from './components/HelloVuex'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloVuex
+  },
+  methods: {
+    add() {
+      this.$store.commit('increment')
+    },
+    sub() {
+      this.$store.commit('decrement')
+    },
+    addBook() {
+      const book = { id: "5", name: "深入理解Java虚拟机", price: 94.6 }
+      // 1. 第一种提交风格
+      this.$store.commit('addBook', book)
+      // 2. 另一种提交风格, 在 mutations 里需要通过 payload 形参对象接收
+      // this.$store.commit({
+      //   type: 'addBook',
+      //   book
+      // })
+    }
+  },  
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
