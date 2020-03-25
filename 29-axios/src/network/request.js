c855586e78c2f9bc24327ee94503a226b04a2ea2
wangxinquan <wangxinquan@anji-tec.com>
2020-03-25 15:15:22 +0800

    拦截器的使用

--- 29-axios/src/network/request.js
+++ 29-axios/src/network/request.js
@@ -5,5 +5,25 @@ export default config => {
     baseURL: 'http://123.207.32.32:8000',
     timeout: 5000
   })
+
+  // 1. 请求拦截
+  //  a. 请求特殊处理
+  //  b. 请求图标
+  //  c. token验证
+  instance.interceptors.request.use(config => {
+    console.log(config)
+    return config
+  }, err => {
+    console.log(err)
+  })
+
+  // 2. 响应拦截
+  instance.interceptors.response.use(res => {
+    console.log(res)
+    return res.data
+  }, err => {
+    console.log(err)
+  })
+
   return instance(config)
 }
