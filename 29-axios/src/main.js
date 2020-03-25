d237c3a581b24e6f5258817497f93c4beae8b904
wangxinquan <wangxinquan@anji-tec.com>
2020-03-25 14:16:17 +0800

    axios全局配置

--- 29-axios/src/main.js
+++ 29-axios/src/main.js
@@ -10,16 +10,39 @@ new Vue({
   render: h => h(App)
 })
 
+axios.defaults.baseURL = 'http://123.207.32.32:8000'
+axios.defaults.timeout = 5000
+
+// 1. axios的基本使用
 axios({
-  url: 'http://123.207.32.32:8000/home/multidata',
+  url: '/home/multidata',
   method: 'get'
 }).then(res => console.log(res))
 
 axios({
-  url: 'http://123.207.32.32:8000/home/data',
+  url: '/home/data',
   method: 'get',
   params: {
     type: 'sell',
     page: 1
   }
 }).then(res => console.log(res))
+
+// 2. axios的并发请求
+axios.all([
+  axios({
+    url: '/home/data',
+    method: 'get',
+    params: {
+      type: 'pop',
+      page: 5
+    }
+  }),
+  axios({
+    url: '/category',
+    method: 'get'
+  })
+]).then(axios.spread((res1, res2) => {
+  console.log(res1)
+  console.log(res2)
+}))
