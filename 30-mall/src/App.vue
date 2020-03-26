a35253622f10263820e0a695d80102658cca1113
wangxinquan <wangxinquan@anji-tec.com>
2020-03-26 15:00:17 +0800

    导入tabbar模块

--- 30-mall/src/App.vue
+++ 30-mall/src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
+  </div>
 </template>
 
 <script>
+import MainTabBar from 'components/content/mainTabBar/MainTabBar'
+
 export default {
-  name: 'App'
+  name: "App",
+  components: {
+    MainTabBar
+  }
 };
 </script>
 
 <style>
-@import url("./assets/css/base.css");
+@import url("assets/css/base.css");
 </style>
