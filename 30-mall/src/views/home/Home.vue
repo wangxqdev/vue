f923e93fb203cd27139504c0c75485bd73dc7e81
wangxinquan <825877689@qq.com>
2020-03-31 21:23:03 +0800

    封装滚动组件

--- 30-mall/src/views/home/Home.vue
+++ 30-mall/src/views/home/Home.vue
@@ -3,64 +3,13 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <home-recommend :recommends="recommends"></home-recommend>
-    <home-feature></home-feature>
-    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick"></tab-control>
-    <goods-list :goods="showGoods"></goods-list>
-
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-      <li>列表41</li>
-      <li>列表42</li>
-      <li>列表43</li>
-      <li>列表44</li>
-      <li>列表45</li>
-      <li>列表46</li>
-      <li>列表47</li>
-      <li>列表48</li>
-      <li>列表49</li>
-      <li>列表50</li>
-    </ul>
+    <scroll class="content">
+      <home-swiper :banners="banners"></home-swiper>
+      <home-recommend :recommends="recommends"></home-recommend>
+      <home-feature></home-feature>
+      <tab-control class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
   </div>
 </template>
 
@@ -70,6 +19,8 @@ import HomeRecommend from "./childcomps/HomeRecommend";
 import HomeFeature from "./childcomps/HomeFeature";
 
 import NavBar from "components/common/navbar/NavBar";
+import Scroll from "components/common/scroll/Scroll"
+
 import TabControl from "components/content/tabcontrol/TabControl";
 import GoodsList from "components/content/goods/GoodsList"
 
@@ -82,6 +33,7 @@ export default {
     HomeRecommend,
     HomeFeature,
     NavBar,
+    Scroll,
     TabControl,
     GoodsList
   },
@@ -150,6 +102,8 @@ export default {
 
 <style scoped>
 #home {
+  position: relative;
+  height: 100vh;
   padding-top: 44px;
 }
 
@@ -168,5 +122,14 @@ export default {
   position: sticky;
   top: 44px;
   z-index: 999;
+}
+
+.content {
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  right: 0;
+  bottom: 49px;
+  left: 0;
 }
 </style>
