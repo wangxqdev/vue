d05860119896b70e52a30f861ce228df2d22778c
wangxinquan <wangxinquan@anji-tec.com>
2020-04-01 16:34:58 +0800

    封装DetailNavBar组件

---
+++ 30-mall/src/views/detail/Detail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="detail">
+    <detail-nav-bar></detail-nav-bar>
+  </div>
+</template>
+
+<script>
+import DetailNavBar from 'views/detail/childcomps/DetailNavBar'
+
+export default {
+  name: 'Detail',
+  components: {
+    DetailNavBar
+  },
+  data() {
+    return {
+      id: null
+    }
+  },
+  created() {
+    this.id = this.$route.params.id 
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- 30-mall/src/views/home/Home.vue
+++ 30-mall/src/views/home/Home.vue
@@ -83,8 +83,8 @@ export default {
     })
   },
   activated() {
-    this.$refs.scroll.scrollTo(0, this.saveY, 0)
     this.$refs.scroll.refresh()
+    this.$refs.scroll.scrollTo(0, this.saveY, 0)
   },
   deactivated() {
     this.saveY = this.$refs.scroll.getScrollY()
