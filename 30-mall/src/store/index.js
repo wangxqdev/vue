ce476a8ec3a0c9a4ea38bf9cafeba1dba96d9090
wangxinquan <825877689@qq.com>
2020-04-07 18:27:16 +0800

    vuex全局管理购物车

--- 30-mall/src/store/index.js
+++ 30-mall/src/store/index.js
@@ -5,8 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cartList: []
   },
   mutations: {
+    addCart(state, payload) {
+      let product = state.cartList.find((item, index, arr) => payload.id === item.id)
+      if (product) {
+        product.count++
+      } else {
+        payload.count = 1
+        state.cartList.push(payload)
+      }
+    }
   },
   actions: {
   },
--- 30-mall/src/views/detail/Detail.vue
+++ 30-mall/src/views/detail/Detail.vue
@@ -118,6 +118,7 @@ export default {
       product.desc = this.goods.desc
       product.price = this.goods.realPrice
       product.id = this.id
+      this.$store.commit('addCart', product)
     }
   }
 }
