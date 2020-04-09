2aea58ed544792aab4961a7d0a531be035edcc02
wangxinquan <wangxinquan@anji-tec.com>
2020-04-09 10:39:09 +0800

    postcss cli4中应用

--- 30-mall/vue.config.js
+++ 30-mall/vue.config.js
@@ -10,5 +10,27 @@ module.exports = {
       }
     },
     devtool: 'source-map'
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require("postcss-px-to-viewport")({
+            unitToConvert: 'px',
+            viewportWidth: 375,
+            viewportHeight: 667,
+            unitPrecision: 5,
+            propList: ['*'],
+            viewportUnit: 'vw',
+            fontViewportUnit: 'vw',
+            selectorBlackList: ['ignore', 'tab-bar', 'tab-bar-item'],
+            minPixelValue: 1,
+            mediaQuery: false,
+            replace: true,
+            exclude: [/TabBar/],
+          })
+        ]
+      }
+    }
   }
 }
